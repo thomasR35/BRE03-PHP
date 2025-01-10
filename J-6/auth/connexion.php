f0be2bb62981cc9fc787e359c21f15191b58afdc
thomasR35 <thomasriou.35@gmail.com>
2025-01-10 11:18:34 +0100

    MaJ dot env

--- J-6/auth/connexion.php
+++ J-6/auth/connexion.php
@@ -1,25 +1,25 @@
 <?php
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-$host = "db.3wa.io";
-$port = "3306";
-$dbname = "thomasriou_php_j6";
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+
 $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 
-$user = "thomasriou";
-$password = "e11d02ae58dcb7466cf6407c21138658";
-
 try {
-    
-    $db = new PDO(
-        $connexionString,
-        $user,
-        $password
-    );
-
-    
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($connexionString, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    
-    die("La connexion a échoué : " . $e->getMessage());
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
 ?>
+

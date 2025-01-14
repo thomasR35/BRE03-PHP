b38e7180e4463bc024071957d736ac0028415618
thomasR35 <thomasriou.35@gmail.com>
2025-01-14 14:31:39 +0100

    test

--- J-2-POO/Mini-Projet/index.php
+++ J-2-POO/Mini-Projet/index.php
@@ -1,3 +1,5 @@
 <?php
-
 require "templates/layout.phtml";
+if (isset($_GET['message'])) {
+    echo "<p>" . htmlspecialchars($_GET['message']) . "</p>";
+}
--- J-2-POO/Mini-Projet/logic/create-user.php
+++ J-2-POO/Mini-Projet/logic/create-user.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../managers/UserManager.class.php';
 require_once __DIR__ . '/../models/User.class.php';
 
+var_dump($_SERVER);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['role'])) {
@@ -12,12 +13,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $userManager->saveUser($user);
 
         echo "Utilisateur enregistré avec succès.";
-
-    } else {
-        echo "Veuillez remplir tous les champs.";
     }
-
-    header('Location: ../index.php');
-    exit;
 }
-

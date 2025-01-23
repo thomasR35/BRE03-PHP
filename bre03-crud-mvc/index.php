711be5850900ebe0f918f67217a13ae6e3699c65
thomasR35 <thomasriou.35@gmail.com>
2025-01-23 14:04:22 +0100

    New

--- bre03-crud-mvc/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/config/autoload.php';
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
-
-$router = new Router();
-$router->route($uri, $method);
